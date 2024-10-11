87d75b67086d93752be3cde62042feab62931be1
Egor Goncharov <forskype1919@gmail.com>
2024-10-11 17:41:42 +0300

    Add english language

--- hello.pas
+++ hello.pas
@@ -2,5 +2,6 @@
   var choice := ReadString('Введите желаемый язык/Enter your language (Eng,Japanese,Spanish,Russian,Italian):');
   case choice of
     'Russian','russian','rus','Rus': Print('Привет, хорошего дня! :)');
+    'english','English','eng','Eng': Print('Hello, have a nice day! :)');
   end;
 end.
