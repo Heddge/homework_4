1cc298f4a7046aee0c30fd0f55b27b73e687f4ac
Egor Goncharov <forskype1919@gmail.com>
2024-10-11 17:38:03 +0300

    Add russian language

---
+++ hello.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var choice := ReadString('Введите желаемый язык/Enter your language (Eng,Japanese,Spanish,Russian,Italian):');
+  case choice of
+    'Russian','russian','rus','Rus': Print('Привет, хорошего дня! :)');
+  end;
+end.
