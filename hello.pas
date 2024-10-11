f1fe8bb53b57f51a24d1e3f0dfc21195d53bdb81
Egor Goncharov <forskype1919@gmail.com>
2024-10-11 17:42:09 +0300

    Add spanish language

--- hello.pas
+++ hello.pas
@@ -3,5 +3,6 @@
   case choice of
     'Russian','russian','rus','Rus': Print('Привет, хорошего дня! :)');
     'english','English','eng','Eng': Print('Hello, have a nice day! :)');
+    'Spanish','spanish','span','Span': Print('Hola, que tengas un buen día! :)');
   end;
 end.
